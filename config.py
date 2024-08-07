2dbe25b4d6e14698f667af89da2622204c860b75
nancy-kataria <katarianancy8@gmail.com>
2024-08-07 11:36:24 -0700

    added config file

---
+++ config.py
@@ -0,0 +1,11 @@
+config = {
+     'bootstrap.servers': 'pkc-12576z.us-west2.gcp.confluent.cloud:9092',
+     'security.protocol': 'SASL_SSL',
+     'sasl.mechanisms': 'PLAIN',
+     'sasl.username': 'RKNPS5H5Q5YVVNFY',
+     'sasl.password': 'bhiwV8SFGuzO5FmtTbo1iXvH9Ke261V1k68Sx0v9Fuf2JAeBfz3KNkkD43dAmUhl'}
+
+sr_config = {
+    'url': 'https://psrc-22y2ny.us-west2.gcp.confluent.cloud',
+    'basic.auth.user.info':'AEISJAAAPXQYZUVK:PMSMZo9p5VTh/3E8/c4nXPHd055CiR0YWvgdOzGKYcB/9M1jkWNhWMN45bwfV1v/'
+}
