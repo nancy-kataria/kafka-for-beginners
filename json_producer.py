ba9807395d139a874f94e29cc44ebde31533de56
nancy-kataria <katarianancy8@gmail.com>
2025-01-06 16:29:01 -0800

    declared class

--- json_producer.py
+++ json_producer.py
@@ -4,3 +4,10 @@ from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from config import config, sr_config
 import time
+
+class Temperature(object):
+    def __init__(self, city, reading, unit, timestamp):
+        self.city = city
+        self.reading = reading
+        self.unit = unit
+        self.timestamp = timestamp
