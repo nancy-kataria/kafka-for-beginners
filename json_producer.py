0ea86ed518e48698b23a352a43ac95d7a94874a2
nancy-kataria <katarianancy8@gmail.com>
2025-01-06 16:29:30 -0800

    defined schema

--- json_producer.py
+++ json_producer.py
@@ -11,3 +11,29 @@ class Temperature(object):
         self.reading = reading
         self.unit = unit
         self.timestamp = timestamp
+
+schema_str = """{
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "Temperature",
+    "description": "Temperature sensor reading",
+    "type": "object",
+    "properties": {
+      "city": {
+        "description": "City name",
+        "type": "string"
+      },
+      "reading": {
+        "description": "Current temperature reading",
+        "type": "number"
+      },
+      "unit": {
+        "description": "Temperature unit (C/F)",
+        "type": "string"
+      },
+      "timestamp": {
+        "description": "Time of reading in ms since epoch",
+        "type": "number"
+      }
+    }
+  }"""
+
