8aca154fdbeec066404107d5f4303bd9385e8d6e
nancy-kataria <katarianancy8@gmail.com>
2025-01-06 16:28:02 -0800

    added imports

---
+++ json_producer.py
@@ -0,0 +1,6 @@
+from confluent_kafka import Producer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONSerializer
+from config import config, sr_config
+import time
