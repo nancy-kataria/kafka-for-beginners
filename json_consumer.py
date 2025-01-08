f7e2abcab300ffd559d7b037ca788c48bf5162b0
nancy-kataria <katarianancy8@gmail.com>
2025-01-07 22:18:52 -0800

    addednconsumer specific configuration properties

--- json_consumer.py
+++ json_consumer.py
@@ -2,3 +2,8 @@ from confluent_kafka import Consumer
 from confluent_kafka.serialization import SerializationContext, MessageField
 from confluent_kafka.schema_registry.json_schema import JSONDeserializer
 from config import config
+
+
+def set_consumer_configs():
+    config['group.id'] = 'temp_group'
+    config['auto.offset.reset'] = 'earliest'
