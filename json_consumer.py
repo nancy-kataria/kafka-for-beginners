c4d37b066aa9c4f41a786ac5e76131497382469b
nancy-kataria <katarianancy8@gmail.com>
2025-01-07 22:19:31 -0800

    class declaration

--- json_consumer.py
+++ json_consumer.py
@@ -7,3 +7,10 @@ from config import config
 def set_consumer_configs():
     config['group.id'] = 'temp_group'
     config['auto.offset.reset'] = 'earliest'
+
+class Temperature(object):
+    def __init__(self, city, reading, unit, timestamp):
+        self.city = city
+        self.reading = reading
+        self.unit = unit
+        self.timestamp = timestamp
