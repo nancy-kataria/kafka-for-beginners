58c357298aec416b447cf20c459f5775c53cd20f
nancy-kataria <katarianancy8@gmail.com>
2025-01-08 23:46:22 -0800

    added function to check if kafka topic exists

--- admin.py
+++ admin.py
@@ -1,2 +1,9 @@
 from confluent_kafka.admin import (AdminClient, NewTopic, ConfigResource)
 from config import config
+
+def topic_exists(admin, topic):
+    metadata = admin.list_topics()
+    for t in iter(metadata.topics.values()):
+        if t.topic == topic:
+            return True
+    return False
