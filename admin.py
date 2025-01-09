2a80c86de3895c914bc7c0ea53446a029e302281
nancy-kataria <katarianancy8@gmail.com>
2025-01-08 23:46:53 -0800

    function to create new topic

--- admin.py
+++ admin.py
@@ -7,3 +7,13 @@ def topic_exists(admin, topic):
         if t.topic == topic:
             return True
     return False
+
+def create_topic(admin, topic):
+    new_topic = NewTopic(topic, num_partitions=6, replication_factor=3)
+    result_dict = admin.create_topics([new_topic])
+    for topic, future in result_dict.items():
+        try:
+            future.result()  # The result itself is None
+            print("Topic {} created".format(topic))
+        except Exception as e:
+            print("Failed to create topic {}: {}".format(topic, e))
